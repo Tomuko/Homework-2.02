5200b4238ce20daba6e831a076066d8af27987e5
Tomuko <vika_stradzina@mail.ru>
2023-01-31 18:32:58 +0300

    Initial Commit

---
+++ Homework 2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.02
+//
+//  Created by Виктория Страдзина on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
